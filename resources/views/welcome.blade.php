7d2b17277ca6779d5da1cae24b2cc18f2110f7f4
Wang Denghan <wangdenghan@WangdeMac-mini.local>
2017-10-27 14:52:27 +0800

    111

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel wdh
                 </div>
 
                 <div class="links">
